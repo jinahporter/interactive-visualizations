7d3ef6f17e1094e32602c1c152062076e087e590
Jinah Porter <jinahporter@gmail.com>
2020-11-14 15:08:27 -0600

    bubble chart - working on it

--- StarterCode/static/app.js
+++ StarterCode/static/app.js
@@ -20,7 +20,53 @@ function getMetadata(outcome) {
     })
 };
 
+function BubbleCharts() {
 
+    d3.json("samples.json").then((importedData) => {
+
+        //name to call otu_ids, otu_labels, sample_values
+        var data = importedData.samples;
+
+        console.log(data[0]);
+
+        var otu_ids = data[0];
+        //var otu_labels = data.map(row => row.otu_labels[0]);
+        //var sample_values = data.map(row => row.sample_values[0]);
+
+        console.log(otu_ids.otu_ids);
+        console.log(otu_ids.otu_labels);
+        console.log(otu_ids.sample_values);
+
+        //create bubble chart
+        var bubble = {
+            x: otu_ids,
+            y: sample_values,
+            text: otu_labels,
+            mode: "markers",
+            marker: {
+                size: sample_values,
+                color: otu_ids
+            }
+        };
+
+        var layout1 = {
+            xaxis: {
+                title: "OTU_IDs"
+            },
+            margin: { 
+                t: 0 
+            },
+            margin: { 
+                t: 30 
+            },
+            hovermode: "closests"
+        };
+
+        var data1 = [bubble];
+
+        Plotly.newPlot("bubble", data1, layout1);
+    });
+}
 
 function pullIDs() {
     d3.json("samples.json").then((importedData) => {
@@ -36,15 +82,21 @@ function pullIDs() {
         getMetadata(getNames1);
 
         //pass my graphs function (getNames1)
+        BubbleCharts(getNames1);
 
     });
 };
 
+
+
 function optionChanged(newID) {
     getMetadata(newID);
+    
+    //pass my graphs function
+    BubbleCharts(newID);
+
 
     
-    //pass my graphs function (getNames1)
 }
 
 pullIDs();
